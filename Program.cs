6fd42aa2bd0964f2576dd7bde0624a4b69c86d9f
Mattias <walde87@msn.com>
2023-03-24 10:05:19 +0100

    Grundkommentarer, pushar för sinnesrons skull.

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 {
     internal class Program
     {
-        static List<SweEngGloss> dictionary;
+        static List<SweEngGloss> dictionary;  //Skapa ny lista? Omvandla till dictionary?
         class SweEngGloss
         {
             public string word_swe, word_eng;
@@ -12,7 +12,7 @@
             }
             public SweEngGloss(string line)
             {
-                string[] words = line.Split('|');
+                string[] words = line.Split('|');                   
                 this.word_swe = words[0]; this.word_eng = words[1];
             }
         }
@@ -22,7 +22,7 @@
             Console.WriteLine("Welcome to the dictionary app!");
             do
             {
-                Console.Write("> ");
+                Console.Write("> ");                                //Lägg till tillgängliga kommandon? ev. hjälp-metod?
                 string[] argument = Console.ReadLine().Split();
                 string command = argument[0];
                 if (command == "quit")
@@ -31,11 +31,11 @@
                 }
                 else if (command == "load")
                 {
-                    if(argument.Length == 2)
+                    if(argument.Length == 2)                      // TODO: Lägg till Try-Catch för "argument[1]"
                     {
                         using (StreamReader sr = new StreamReader(argument[1]))
                         {
-                            dictionary = new List<SweEngGloss>(); // Empty it!
+                            dictionary = new List<SweEngGloss>(); // Empty it! TODO: Byt till "Clear"
                             string line = sr.ReadLine();
                             while (line != null)
                             {
@@ -49,7 +49,7 @@
                     {
                         using (StreamReader sr = new StreamReader(defaultFile))
                         {
-                            dictionary = new List<SweEngGloss>(); // Empty it!
+                            dictionary = new List<SweEngGloss>(); // Empty it! TODO: Byt till "Clear"
                             string line = sr.ReadLine();
                             while (line != null)
                             {
@@ -64,27 +64,27 @@
                 {
                     foreach(SweEngGloss gloss in dictionary)
                     {
-                        Console.WriteLine($"{gloss.word_swe,-10}  - {gloss.word_eng,-10}");
+                        Console.WriteLine($"{gloss.word_swe,-10}  - {gloss.word_eng,-10}"); // -10? Återkom efter testkör
                     }
                 }
                 else if (command == "new")
                 {
                     if (argument.Length == 3)
                     {
-                        dictionary.Add(new SweEngGloss(argument[1], argument[2]));
+                        dictionary.Add(new SweEngGloss(argument[1], argument[2]));          //Lirar mot första buildern
                     }
-                    else if(argument.Length == 1)
+                    else if(argument.Length == 1) //Ändra variablenamn till lättläsligt
                     {
-                        Console.WriteLine("Write word in Swedish: ");
+                        Console.WriteLine("Write word in Swedish: "); //Kod lika som på delete, bryt ut och kalla på?
                         string s = Console.ReadLine();
                         Console.Write("Write word in English: ");
                         string e = Console.ReadLine();
                         dictionary.Add(new SweEngGloss(s, e));
                     }
                 }
-                else if (command == "delete")
+                else if (command == "delete") 
                 {
-                    if (argument.Length == 3)
+                    if (argument.Length == 3) //TBD: Lägg till kod som ignorerar versaler/gemener, samt try-catch
                     {
                         int index = -1;
                         for (int i = 0; i < dictionary.Count; i++) {
@@ -94,9 +94,9 @@
                         }
                         dictionary.RemoveAt(index);
                     }
-                    else if (argument.Length == 1)
+                    else if (argument.Length == 1) //Fixa variabelnamn,
                     {
-                        Console.WriteLine("Write word in Swedish: ");
+                        Console.WriteLine("Write word in Swedish: "); //Kod lika som på new, bryt ut och kalla på?
                         string s = Console.ReadLine();
                         Console.Write("Write word in English: ");
                         string e = Console.ReadLine();
@@ -112,9 +112,9 @@
                 }
                 else if (command == "translate")
                 {
-                    if (argument.Length == 2)
+                    if (argument.Length == 2)  //Try-catch, 
                     {
-                        foreach(SweEngGloss gloss in dictionary)
+                        foreach(SweEngGloss gloss in dictionary)  //Bryt ut foreach till båda translate.
                         {
                             if (gloss.word_swe == argument[1])
                                 Console.WriteLine($"English for {gloss.word_swe} is {gloss.word_eng}");
