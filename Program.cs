58a66a3b8a7a68e0bd4404e839fdda2dc00fa8a5
Mattias <walde87@msn.com>
2023-03-24 10:52:50 +0100

    Load metod, utbruten och testad.

--- Program.cs
+++ Program.cs
@@ -12,57 +12,36 @@
             }
             public SweEngGloss(string line)
             {
-                string[] words = line.Split('|');                   
+                string[] words = line.Split('|');
                 this.word_swe = words[0]; this.word_eng = words[1];
             }
         }
         static void Main(string[] args)
         {
-            string defaultFile = "..\\..\\..\\dict\\sweeng.lis";
             Console.WriteLine("Welcome to the dictionary app!");
             do
             {
                 Console.Write("> ");                                //Lägg till tillgängliga kommandon? ev. hjälp-metod?
                 string[] argument = Console.ReadLine().Split();
                 string command = argument[0];
-                if (command == "quit")
+                if (command == "quit") //Fixa avslut
                 {
                     Console.WriteLine("Goodbye!");
                 }
                 else if (command == "load")
                 {
-                    if(argument.Length == 2)                      // TODO: Lägg till Try-Catch för "argument[1]"
+                    if (argument.Length == 2)                      // TODO: Lägg till Try-Catch för "argument[1]"
                     {
-                        using (StreamReader sr = new StreamReader(argument[1]))
-                        {
-                            dictionary.Clear();
-                            string line = sr.ReadLine();
-                            while (line != null)
-                            {
-                                SweEngGloss gloss = new SweEngGloss(line);
-                                dictionary.Add(gloss);
-                                line = sr.ReadLine();
-                            }
-                        }
+                        Load(argument);
                     }
-                    else if(argument.Length == 1)
+                    else if (argument.Length == 1)
                     {
-                        using (StreamReader sr = new StreamReader(defaultFile))
-                        {
-                            dictionary.Clear();
-                            string line = sr.ReadLine();
-                            while (line != null)
-                            {
-                                SweEngGloss gloss = new SweEngGloss(line);
-                                dictionary.Add(gloss);
-                                line = sr.ReadLine();
-                            }
-                        }
+                        Load(argument);
                     }
                 }
                 else if (command == "list")
                 {
-                    foreach(SweEngGloss gloss in dictionary)
+                    foreach (SweEngGloss gloss in dictionary)
                     {
                         Console.WriteLine($"{gloss.word_swe,-10}  - {gloss.word_eng,-10}"); // -10? Återkom efter testkör
                     }
@@ -73,7 +52,7 @@
                     {
                         dictionary.Add(new SweEngGloss(argument[1], argument[2]));          //Lirar mot första buildern
                     }
-                    else if(argument.Length == 1) //Ändra variablenamn till lättläsligt
+                    else if (argument.Length == 1) //Ändra variablenamn till lättläsligt
                     {
                         Console.WriteLine("Write word in Swedish: "); //Kod lika som på delete, bryt ut och kalla på?
                         string s = Console.ReadLine();
@@ -82,12 +61,13 @@
                         dictionary.Add(new SweEngGloss(s, e));
                     }
                 }
-                else if (command == "delete") 
+                else if (command == "delete")
                 {
                     if (argument.Length == 3) //TBD: Lägg till kod som ignorerar versaler/gemener, samt try-catch
                     {
                         int index = -1;
-                        for (int i = 0; i < dictionary.Count; i++) {
+                        for (int i = 0; i < dictionary.Count; i++)
+                        {
                             SweEngGloss gloss = dictionary[i];
                             if (gloss.word_swe == argument[1] && gloss.word_eng == argument[2])
                                 index = i;
@@ -114,7 +94,7 @@
                 {
                     if (argument.Length == 2)  //Try-catch, 
                     {
-                        foreach(SweEngGloss gloss in dictionary)  //Bryt ut foreach till båda translate.
+                        foreach (SweEngGloss gloss in dictionary)  //Bryt ut foreach till båda translate.
                         {
                             if (gloss.word_swe == argument[1])
                                 Console.WriteLine($"English for {gloss.word_swe} is {gloss.word_eng}");
@@ -142,5 +122,25 @@
             }
             while (true);
         }
+
+        private static void Load(string[] argument)
+        {
+
+            string input = "..\\..\\..\\dict\\sweeng.lis";
+            if (argument.Length == 2) input = argument[1];
+            using (StreamReader sr = new StreamReader(input))
+            {
+                dictionary.Clear();
+                string line = sr.ReadLine();
+                while (line != null)
+                {
+                    SweEngGloss gloss = new SweEngGloss(line);
+                    dictionary.Add(gloss);
+                    line = sr.ReadLine();
+                }
+            }
+
+        }
     }
 }
+
